b002642a49a1447287731350fd335bc0561585ff
s0ftik3 <slava021323@gmail.com>
2020-07-25 02:11:40 +0600

    add new project - discord. Edited news project. Some minor JS updates.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,11 @@
 var request = new XMLHttpRequest();
-request.open('GET', 'https://discordapp.com/api/guilds/718067621412732959/widget.json', true);
+request.open('GET', 'https://discordapp.com/api/guilds/718067621412732959/widget.json');
 
 request.onload = function() {
   if (this.status >= 200 && this.status < 400) {
     var data = JSON.parse(this.response);
     var me = data.members.find(u => u.username === 'softik');
     var status = me.status;
-
     switch (status) {
       case 'dnd': 
         document.getElementById('discordState').style.background = '#f04747';
@@ -22,9 +21,6 @@ request.onload = function() {
         break;
     }
   }
-};
-
-request.onerror = function() {
 };
 
 request.send();
---
+++ projects/discord/main.js
@@ -0,0 +1,51 @@
+function openButton() {
+    document.getElementById('sendButton').disabled = false;
+}
+
+function hexToDecimal(hex) {
+    return parseInt(hex.replace("#",""), 16)
+}
+
+function currentTime() {
+    var date = new Date();
+    return date.toISOString();
+}
+
+function success() {
+    document.getElementById('message').value = '';
+    document.getElementById('message').placeholder = 'Successfully sent!';
+    window.setTimeout(() => {
+        document.getElementById('message').placeholder = 'What do you want to say?';
+    }, 1500);
+}
+
+function fail() {
+    document.getElementById('message').value = '';
+    document.getElementById('message').placeholder = 'Bad Request';
+    window.setTimeout(() => {
+        document.getElementById('message').placeholder = 'Open this page later';
+    }, 1500);
+}
+
+function sendMessage() {
+    var text = document.getElementById('message').value;
+    if (!text) return;
+    var request = new XMLHttpRequest();
+    request.open('POST', 'https://discordapp.com/api/webhooks/736285089301921822/-vEASfRw6C2E4a3YVQbSs-1mIjTHhfKkrK8iY5QCZU7XadgIj-q5amiZyHKH5lySPNcy');
+    request.setRequestHeader('Content-type', 'application/json');
+    var params = {
+        embeds: [{
+            title: 'Send your message',
+            url: 'https://sloova.xyz/projects/discord/',
+            fields: [{
+                name: 'Message :love_letter:',
+                value: `\`\`\`${text}\`\`\``
+            }],
+            color: hexToDecimal("#4d7ae8"),
+            timestamp: currentTime()
+        }]
+    };
+    request.send(JSON.stringify(params));
+    success();
+    document.getElementById('sendButton').disabled = true;
+}
